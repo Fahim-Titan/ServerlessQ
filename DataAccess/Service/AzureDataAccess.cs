e6320f1fa53dd9bbe3309ce1050de9d8331e814f
masum_fahim <masum_fahim@nidaansystems.com>
2024-09-20 23:41:46 -0700

    feat: add services

---
+++ DataAccess/Service/AzureDataAccess.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.Interface;
+using DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Service
+{
+    internal class AzureDataAccess: IDataAccess
+    {
+        public AzureDataAccess() { }
+
+        public Task SaveData(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/Service/SQLiteDatabase.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.Interface;
+using DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Service
+{
+    internal class SQLiteDatabase: IDataAccess
+    {
+        private readonly string _databasePath;
+        public SQLiteDatabase()
+        {
+            _databasePath = Path.Combine(Directory.GetCurrentDirectory(), "data.db");
+        }
+
+        public Task SaveData(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
