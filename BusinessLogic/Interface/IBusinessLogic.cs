1def15f1c34daf2e15da16f7368b1be032247d46
masum_fahim <masum_fahim@nidaansystems.com>
2024-09-21 19:20:02 -0700

    feat: add interface IBusinessLogic and business class

---
+++ BusinessLogic/Interface/IBusinessLogic.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BusinessLogic.Interface
+{
+    public interface IBusinessLogic
+    {
+        Task SaveData(string firstName, string lastName);
+        Task PublishData(Person data);
+    }
+}
---
+++ BusinessLogic/Service/Business.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLogic.Interface;
+using DataAccess.Interface;
+using DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Service
+{
+    public class Business: IBusinessLogic
+    {
+        private readonly IRepository _repo;
+        private readonly IMessageQueue _msgQueue;
+        public Business(IRepository dataAccess, IMessageQueue messageQueue)
+        {
+            _repo = dataAccess;
+        }
+
+        public Task PublishData(Person data)
+        {
+            _msgQueue.SendMessage(data);
+            throw new NotImplementedException();
+        }
+
+        public Task SaveData(string firstName, string lastName)
+        {
+            //TODO: add validation
+
+            Person person = new Person();
+            person.FirstName = firstName;
+            person.LastName = lastName;
+            var dbData = _repo.SaveData(person);
+            
+            //TODO: Return result
+            throw new NotImplementedException();
+        }
+    }
+}
