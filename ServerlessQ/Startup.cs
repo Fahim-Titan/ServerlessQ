8291f443564bfac9d60fb8a17673fbba1a881c87
masum_fahim <masum_fahim@nidaansystems.com>
2024-09-21 19:20:32 -0700

    feat: add startup in ServerlessQ

---
+++ ServerlessQ/Startup.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLogic.Interface;
+using BusinessLogic.Service;
+using DataAccess.Interface;
+using DataAccess.Service;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerlessQ
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<IRepository, SQLiteDatabase>();
+            builder.Services.AddSingleton<IMessageQueue, AzureDataAccess>();
+            builder.Services.AddSingleton<IBusinessLogic>(sp =>
+            {
+                var repo = sp.GetService<IRepository>();
+                var msgQueue = sp.GetService<IMessageQueue>();
+                return new Business(repo, msgQueue);
+            });
+        }
+    }
+}
