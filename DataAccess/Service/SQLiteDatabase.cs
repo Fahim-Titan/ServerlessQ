44b3665624023e4e6c972375d8c6a6a59db6d07a
masum_fahim <masum_fahim@nidaansystems.com>
2024-09-22 11:54:12 -0700

    feat: create table if not exist | add sql to save data

--- DataAccess/Service/SQLiteDatabase.cs
+++ DataAccess/Service/SQLiteDatabase.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccess.Interface;
 using DataAccess.Model;
+using Microsoft.Data.Sqlite;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,11 +15,58 @@ namespace DataAccess.Service
         public SQLiteDatabase()
         {
             _databasePath = Path.Combine(Directory.GetCurrentDirectory(), "data.db");
+            CreateTableIfNotExists();
         }
 
-        public Task SaveData(Person person)
+        private void CreateTableIfNotExists()
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (var connection = new SqliteConnection($"Data Source={_databasePath}"))
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = @"
+                        CREATE TABLE IF NOT EXISTS Person (
+                            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            FirstName TEXT NOT NULL,
+                            LastName TEXT NOT NULL
+                        );
+                    ";
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error Occured during the table creation", ex);
+            }
         }
+
+        public async Task<Person> SaveData(Person person)
+        {
+            try
+            {
+                using (var connection = new SqliteConnection($"Data Source={_databasePath}"))
+                {
+                    await connection.OpenAsync();
+                    var command = connection.CreateCommand();
+                    command.CommandText = @"
+                        INSERT INTO Person (FirstName, LastName)
+                        VALUES (@FirstName, @LastName);
+                        SELECT last_insert_rowid();
+                    ";
+                    command.Parameters.AddWithValue("@FirstName", person.FirstName);
+                    command.Parameters.AddWithValue("@LastName", person.LastName);
+                    var result = await command.ExecuteScalarAsync();
+                    person.Id = Convert.ToInt32(result);
+                    return person;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occured in Saving data in SQL DB", ex);
+            }
+        }
+
     }
 }
